a8dda182dac82cc07ff286a6cafcc62d4a203829
hjzhhhj <jhj090120@gmail.com>
2025-06-03 22:32:14 +0900

    [feat] 학교 코드 관리

--- src/utils/schoolCodeMap.ts
+++ src/utils/schoolCodeMap.ts
@@ -1,11 +1,30 @@
-export const SCHOOL_CODE_MAP = {
+export const SCHOOL_CODE_MAP: { [key: string]: { atptOfcdcScCode: string; sdSchulCode: string } } = {
+    "설악중학교": {
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7863055",
+    },
+    "속초중학교": {
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7863057",
+    },
+    "해랑중학교": {
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7863058",
+    },
+    "설온중학교": {
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7863056",
+    },
     "설악고등학교": {
-      atptOfcdcScCode: "K10",  
-      sdSchulCode: "7801151",  
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7801148",
     },
     "속초고등학교": {
-      atptOfcdcScCode: "K10",
-      sdSchulCode: "7801152",
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7801152",
     },
-    // 나머지도 추가!
-  };
+    "속초여자고등학교": {
+        atptOfcdcScCode: "K10",
+        sdSchulCode: "7801153",
+    },
+};
