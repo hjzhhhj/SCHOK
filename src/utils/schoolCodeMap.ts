2ac76daec532b5a09ccf1c41c5714634394640e0
hjzhhhj <jhj090120@gmail.com>
2025-06-10 05:05:29 +0900

    [feat] : 학교 위도 경도 추가

--- src/utils/schoolCodeMap.ts
+++ src/utils/schoolCodeMap.ts
@@ -1,37 +1,51 @@
-export const SCHOOL_CODE_MAP: { [key: string]: { atptOfcdcScCode: string; sdSchulCode: string; schoolType: '중학교' | '고등학교' } } = {
+export const SCHOOL_CODE_MAP: { [key: string]: { atptOfcdcScCode: string; sdSchulCode: string; schoolType: '중학교' | '고등학교'; schoolLatitude: number; schoolLongitude: number; } } = {
     "설악중학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7863055",
-        schoolType: "중학교"
+        schoolType: "중학교",
+        schoolLatitude: 38.207089,
+        schoolLongitude: 128.578037 
     },
     "속초중학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7863057",
-        schoolType: "중학교"
+        schoolType: "중학교",
+        schoolLatitude: 38.204374, 
+        schoolLongitude: 128.574389 
     },
     "해랑중학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7863058",
-        schoolType: "중학교"
+        schoolType: "중학교",
+        schoolLatitude: 38.208309, 
+        schoolLongitude: 128.585031 
     },
     "설온중학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7863056",
-        schoolType: "중학교"
+        schoolType: "중학교",
+        schoolLatitude: 38.200543,
+        schoolLongitude: 128.566835
     },
     "설악고등학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7801148",
-        schoolType: "고등학교"
+        schoolType: "고등학교",
+        schoolLatitude: 38.185676,
+        schoolLongitude: 128.573626 
     },
     "속초고등학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7801152",
-        schoolType: "고등학교"
+        schoolType: "고등학교",
+        schoolLatitude: 38.225625,
+        schoolLongitude: 128.583675
     },
     "속초여자고등학교": {
         atptOfcdcScCode: "K10",
         sdSchulCode: "7801153",
-        schoolType: "고등학교"
+        schoolType: "고등학교",
+        schoolLatitude: 38.199757, 
+        schoolLongitude: 128.568464
     },
 };
